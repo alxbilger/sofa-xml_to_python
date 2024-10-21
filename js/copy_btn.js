434281e1be4d7cc6c85a15d350b72ccec11e6588
Alex Bilger <alexbilger0@gmail.com>
2024-10-21 10:23:50 +0200

    copy button

---
+++ js/copy_btn.js
@@ -0,0 +1,13 @@
+﻿import {pythonEditor} from "./editors.js";
+
+function copy_content() {
+    if (pythonEditor) {
+        navigator.clipboard.writeText(pythonEditor.getValue());
+        console.log('Copied!');
+    }
+}
+
+let copy_btn = document.getElementById('copy_btn');
+if (copy_btn) {
+    copy_btn.addEventListener('click', copy_content);
+}
